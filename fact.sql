366782bfa5258de85ad5851c78d1656858986e07
Ashwani Jha <ashwanijha04@gmail.com>
2017-12-22 00:39:40 +0530

    Update fact.sql

--- fact.sql
+++ fact.sql
@@ -1,16 +1,20 @@
-declare
-    n number;
-    fac number:=1;
-    i number;
- 
-begin
-    n:=&n;
- 
-    for i in 1..n
-    loop
-        fac:=fac*i;
-    end loop;
- 
-    dbms_output.put_line('factorial='||fac);
+set serveroutput on;
+
+create or replace PROCEDURE fact as
+n number;
+i number;
+fac number:=1;
+
+BEGIN
+n:=5;
+for i in 1..n
+loop
+fac:=fac * i;
+end loop;
+
+dbms_output.put_line('Factorial of '|| n || ' is: '|| fac);
+
 end;
 /
+
+execute fact;
